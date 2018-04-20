ec276a06e4e3a0c6611c70dd994673411d9914f6
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-04-20 20:38:42 +0700

    Merge train and validation sets

--- create_data_folders.py
+++ create_data_folders.py
@@ -12,9 +12,9 @@ valid_images = sorted(os.listdir('validation/'))
 
 classes = sorted(set([int(image.split('_')[1].split('.')[0])
                       for image in train_images]))
-for cls in classes:
-    os.makedirs(os.path.join('data/train', str(cls)))
-    os.makedirs(os.path.join('data/validation', str(cls)))
+# for cls in classes:
+#     os.makedirs(os.path.join('data/train', str(cls)))
+#     os.makedirs(os.path.join('data/validation', str(cls)))
 
 
 def copyFile(src, dest):
@@ -26,20 +26,26 @@ def copyFile(src, dest):
         print('Error: %s' % e.strerror)
 
 
-if not os.path.exists('data/train'):
-    os.makedirs('data/train')
+# if not os.path.exists('data/train'):
+#     os.makedirs('data/train')
 
-if not os.path.exists('data/validation'):
-    os.makedirs('data/validation')
+# if not os.path.exists('data/validation'):
+#     os.makedirs('data/validation')
 
 for image in tqdm(valid_images):
     cls = image.split('_')[1].split('.')[0]
     old_path = os.path.join('validation/', image)
-    new_path = os.path.join('data/validation', cls)
-    copyFile(old_path, new_path)
-
-for image in tqdm(train_images):
-    cls = image.split('_')[1].split('.')[0]
-    old_path = os.path.join('train/', image)
     new_path = os.path.join('data/train', cls)
-    copyFile(old_path, new_path)
+    shutil.copy2(old_path, os.path.join(new_path, 'val_'+image))
+
+# for image in tqdm(valid_images):
+#     cls = image.split('_')[1].split('.')[0]
+#     old_path = os.path.join('validation/', image)
+#     new_path = os.path.join('data/validation', cls)
+#     shutil.copy2(old_path, new_path)
+
+# for image in tqdm(train_images):
+#     cls = image.split('_')[1].split('.')[0]
+#     old_path = os.path.join('train/', image)
+#     new_path = os.path.join('data/train', cls)
+#     copyFile(old_path, new_path)
