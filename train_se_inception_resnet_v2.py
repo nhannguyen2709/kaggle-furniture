0b5bd01aecf3032794b24e41622e7609df72c4ec
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-04-27 16:57:31 +0700

    Server

--- train_se_inception_resnet_v2.py
+++ train_se_inception_resnet_v2.py
@@ -25,22 +25,19 @@ x_from_train_images, y_from_train_images = get_image_paths_and_labels(
 x_from_val_images, y_from_val_images = get_image_paths_and_labels(
     data_dir='data/validation/')
 
-sss = StratifiedShuffleSplit(n_splits=1, test_size=0.8, random_state=2)
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=2)
 for val_index, minival_index in sss.split(x_from_val_images, y_from_val_images):
     x_from_minival_images, y_from_minival_images = x_from_val_images[
         minival_index], y_from_val_images[minival_index]
     x_from_val_images, y_from_val_images = x_from_val_images[
         val_index], y_from_val_images[val_index]
 
-x_from_train_val_images = np.append(x_from_train_images, x_from_val_images)
-y_from_train_val_images = np.append(y_from_train_images, y_from_val_images)
-
 # 5-fold cross-validation
-input_shape = (560, 560)
-batch_size = 16
-epochs = 10
-num_workers = 16
-n_splits = 5
+input_shape = (299, 299)
+batch_size = 32
+epochs = 6
+num_workers = 10
+n_splits = 7
 n_repeats = 1
 num_gpus = 2
 rskf = RepeatedStratifiedKFold(
@@ -48,20 +45,21 @@ rskf = RepeatedStratifiedKFold(
 
 fold = 1
 for train_index, test_index in rskf.split(
-        x_from_train_val_images, y_from_train_val_images):
+        x_from_train_images, y_from_train_images):
     fold += 1
-    x_train, x_valid = x_from_train_val_images[train_index], x_from_train_val_images[test_index]
-    y_train, y_valid = y_from_train_val_images[train_index], y_from_train_val_images[test_index]
+    x_train, x_valid = x_from_train_images[train_index], x_from_train_images[test_index]
+    y_train, y_valid = y_from_train_images[train_index], y_from_train_images[test_index]
+    print('\nFold {}'.format(fold))
     print('Found {} images belonging to {} classes'.format(len(x_train), 128))
     print('Found {} images belonging to {} classes'.format(len(x_valid), 128))
 
     train_generator = FurnituresDatasetWithAugmentation(
-        x_train, y_train, batch_size=batch_size, input_shape=input_shape)
+        x_train, y_train, 
+        batch_size=batch_size, input_shape=input_shape)
     valid_generator = FurnituresDatasetNoAugmentation(
-        x_valid, y_valid, batch_size=batch_size,
-        input_shape=input_shape)
-    minival_generator = FurnituresDatasetNoAugmentation(x_from_minival_images, y_from_minival_images,
-                                                        batch_size=32, input_shape=input_shape)
+        x_valid, y_valid, 
+        batch_size=batch_size, input_shape=input_shape)
+
     weights_path = 'checkpoint/se_inception_resnet_v2/fold{}.weights.best.hdf5'.format(
         fold)
     save_best = ExponentialMovingAverage(filepath=weights_path,
@@ -75,6 +73,8 @@ for train_index, test_index in rskf.split(
     
     # # multi-gpu train
     base_model = build_se_inception_resnet_v2()
+    # if os.path.exists(weights_path):
+    #     base_model.load_weights(weights_path)
     base_model.compile(optimizer=RMSprop(lr=4.5e-2),
                        loss='categorical_crossentropy',
                        metrics=['acc'])
@@ -86,16 +86,25 @@ for train_index, test_index in rskf.split(
                                  validation_data=valid_generator,
                                  workers=num_workers)
     
+    del train_generator, valid_generator
+
+    val_generator = FurnituresDatasetWithAugmentation(
+        x_from_val_images, y_from_val_images, batch_size=batch_size, input_shape=input_shape)
+    minival_generator = FurnituresDatasetNoAugmentation(
+        x_from_minival_images, y_from_minival_images,
+        batch_size=32, input_shape=input_shape)
+    print('Found {} images belonging to {} classes'.format(len(x_from_val_images), 128))
+    print('Found {} images belonging to {} classes'.format(len(x_from_minival_images), 128))
     base_model.compile(optimizer=RMSprop(lr=4.5e-3),
                        loss='categorical_crossentropy',
                        metrics=['acc'])
     parallel_model.compile(optimizer=RMSprop(lr=4.5e-3), loss='categorical_crossentropy', metrics=['acc'])
     finetune_lr_scheduler = LearningRateScheduler(schedule=finetune_lr_schedule, verbose=1)
     callbacks = [finetune_lr_scheduler, save_best]
-    parallel_model.fit_generator(generator=minival_generator,
+    parallel_model.fit_generator(generator=val_generator,
                                  epochs=epochs,
                                  callbacks=callbacks,
-                                 validation_data=valid_generator,
+                                 validation_data=minival_generator,
                                  workers=num_workers)
 
     # # single-gpu train
@@ -107,7 +116,6 @@ for train_index, test_index in rskf.split(
     #               metrics=['acc'])
     # model.fit_generator(generator=train_generator,
     #                     epochs=epochs,
-    #                     steps_per_epoch=1,
     #                     callbacks=callbacks,
     #                     validation_data=valid_generator,
     #                     workers=num_workers)
@@ -115,10 +123,10 @@ for train_index, test_index in rskf.split(
     # model.compile(optimizer=RMSprop(lr=4.5e-3), loss='categorical_crossentropy', metrics=['acc'])
     # finetune_lr_scheduler = LearningRateScheduler(schedule=finetune_lr_schedule, verbose=1)
     # callbacks = [finetune_lr_scheduler, save_best]
-    # model.fit_generator(generator=minival_generator,
-    #                     epochs=epochs,
-    #                     callbacks=callbacks,
-    #                     validation_data=valid_generator,
-    #                     workers=num_workers)
+    # model.fit_generator(generator=val_generator,
+                        #   epochs=epochs,
+                        #   callbacks=callbacks,
+                        #   validation_data=minival_generator,
+                        #   workers=num_workers)
 
     K.clear_session()
