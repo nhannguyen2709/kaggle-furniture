6b904f0c0928be47bd1e9320e0994cedf7e16993
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-04-29 14:23:12 +0700

    Rewrite as functions

--- create_data_folders.py
+++ create_data_folders.py
@@ -1,22 +1,8 @@
+import cv2
+from imgaug import augmenters as iaa
 import os
 import shutil
 from tqdm import tqdm
-
-num_train_samples = len(os.listdir('train/'))
-num_valid_samples = len(os.listdir('validation/'))
-print('Train images: {}, validation images: {}'.format(
-    num_train_samples, num_valid_samples))
-
-train_images = sorted(os.listdir('train/'))
-valid_images = sorted(os.listdir('validation/'))
-
-classes = sorted(set([int(image.split('_')[1].split('.')[0])
-                      for image in train_images]))
-for cls in classes:
-    shutil.rmtree(os.path.join('data/train', str(cls)))
-    os.makedirs(os.path.join('data/train', str(cls)))
-    # os.makedirs(os.path.join('data/validation', str(cls)))
-
 
 def copyFile(src, dest):
     try:
@@ -26,27 +12,62 @@ def copyFile(src, dest):
     except IOError as e:
         print('Error: %s' % e.strerror)
 
+def create_train_and_val_folders():
+    num_train_samples = len(os.listdir('train/'))
+    num_valid_samples = len(os.listdir('validation/'))
+    print('Train images: {}, validation images: {}'.format(
+        num_train_samples, num_valid_samples))
+    train_images = sorted(os.listdir('train/'))
+    valid_images = sorted(os.listdir('validation/'))
+    labels_idxs = sorted(set([int(image.split('_')[1].split('.')[0])
+                        for image in train_images]))
 
-# if not os.path.exists('data/train'):
-#     os.makedirs('data/train')
+    if not os.path.exists('data/train'):
+        os.makedirs('data/train')
+    if not os.path.exists('data/validation'):
+        os.makedirs('data/validation')
+    for label_idx in labels_idxs:
+        shutil.rmtree(os.path.join('data/train', str(label_idx)))
+        os.makedirs(os.path.join('data/train', str(label_idx)))
+        os.makedirs(os.path.join('data/validation', str(label_idx)))
 
-# if not os.path.exists('data/validation'):
-#     os.makedirs('data/validation')
+    for image in tqdm(train_images):
+        label_idx = image.split('_')[1].split('.')[0]
+        old_path = os.path.join('train/', image)
+        new_path = os.path.join('data/train', label_idx)
+        copyFile(old_path, new_path)
+    for image in tqdm(valid_images):
+        label_idx = image.split('_')[1].split('.')[0]
+        old_path = os.path.join('validation/', image)
+        new_path = os.path.join('data/validation', label_idx)
+        copyFile(old_path, new_path)
 
-# for image in tqdm(valid_images):
-#     cls = image.split('_')[1].split('.')[0]
-#     old_path = os.path.join('validation/', image)
-#     new_path = os.path.join('data/train', cls)
-#     shutil.copy2(old_path, os.path.join(new_path, 'val_'+image))
+def crop_and_save_imgs(percent_cropped=0.1):
+    for img_folder in sorted(os.listdir('data/train')):
+        img_folder_path = os.path.join('data/train', img_folder)
+        print('\n {}'.format(img_folder_path))
+        for img_file in tqdm(sorted(os.listdir(img_folder_path))):
+            img_file_path = os.path.join(img_folder_path, img_file)
+            img_arr = cv2.imread(img_file_path)
+            img_arr = iaa.Crop(px=(int(percent_cropped*img_arr.shape[0]),
+                                    int(percent_cropped*img_arr.shape[1]),
+                                    int(percent_cropped*img_arr.shape[0]),
+                                    int(percent_cropped*img_arr.shape[1])),
+                                keep_size=False).augment_image(img_arr)
+            cv2.imwrite(img_file_path, img_arr)
 
-# for image in tqdm(valid_images):
-#     cls = image.split('_')[1].split('.')[0]
-#     old_path = os.path.join('validation/', image)
-#     new_path = os.path.join('data/validation', cls)
-#     shutil.copy2(old_path, new_path)
+    for img_folder in sorted(os.listdir('data/validation')):
+        img_folder_path = os.path.join('data/validation', img_folder)
+        print('\n {}'.format(img_folder_path))
+        for img_file in tqdm(sorted(os.listdir(img_folder_path))):
+            img_file_path = os.path.join(img_folder_path, img_file)
+            img_arr = cv2.imread(img_file_path)
+            img_arr = iaa.Crop(px=(int(percent_cropped*img_arr.shape[0]),
+                                    int(percent_cropped*img_arr.shape[1]),
+                                    int(percent_cropped*img_arr.shape[0]),
+                                    int(percent_cropped*img_arr.shape[1])),
+                                keep_size=False).augment_image(img_arr)
+            cv2.imwrite(img_file_path, img_arr)
 
-for image in tqdm(train_images):
-    cls = image.split('_')[1].split('.')[0]
-    old_path = os.path.join('train/', image)
-    new_path = os.path.join('data/train', cls)
-    copyFile(old_path, new_path)
+if __name__=='__main__':
+    crop_and_save_imgs(0.1)
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import os
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications.densenet import DenseNet201
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from keras.layers import Dense, GlobalMaxPooling2D
+from keras.layers import Dense, Dropout, GlobalMaxPooling2D
 from keras.models import Model
 from keras.optimizers import Adam, SGD
 from keras.preprocessing.image import ImageDataGenerator
@@ -32,48 +32,22 @@ class MultiGPUModel(Model):
         return super(MultiGPUModel, self).__getattribute__(attrname)
 
 
-def evaluate_model_no_data_augmentation(valid_dir, input_shape, checkpoint_dir, base_model, model_name, num_workers):
-    valid_datagen = ImageDataGenerator(
-        rescale=1. / 255)
-    valid_generator = valid_datagen.flow_from_directory(
-        valid_dir,
-        batch_size=64,
-        target_size=input_shape,
-        class_mode='categorical',
-        shuffle=False)
-    x = GlobalMaxPooling2D(name='max_pool')(base_model.layers[-1].output)
-    x = Dense(128, activation='softmax', name='predictions')(x)
-    model = Model(inputs=base_model.layers[0].input, outputs=x)
-
-    model_checkpoint_dir = os.path.join(checkpoint_dir, model_name)
-    if not os.path.exists(model_checkpoint_dir):
-        os.makedirs(model_checkpoint_dir)
-
-    weights_filenames = sorted(os.listdir(model_checkpoint_dir))
-    for weights_filename in weights_filenames:
-        model.load_weights(os.path.join(model_checkpoint_dir, weights_filename))
-        model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['acc'])
-        val_loss, val_acc = model.evaluate_generator(valid_generator, workers=num_workers)
-        print(weights_filename, val_loss, val_acc)
-
-
 def build_densenet_201(verbose=False):
     model = DenseNet201(include_top=False, pooling='max')
-    output = Dense(128, activation='softmax', name='predictions')(model.layers[-1].output)
+    x = Dense(1024, activation='relu', name='fc_layer1')(model.layers[-1].output)
+    x = Dropout(0.4)(x)
+    output = Dense(128, activation='softmax', name='predictions')(x)
     model = Model(inputs=model.layers[0].input, outputs=output)
     finetuned_layers_names = [
-        # 'conv5_block31_1_conv',
-        # 'conv5_block31_2_conv',
-        'conv5_block32_1_conv',
-        'conv5_block32_2_conv',
+        # 'conv5_block32_1_conv',
+        # 'conv5_block32_2_conv',
+        'fc_layer1',
         'predictions']
     finetuned_layers = [model.get_layer(name=layer_name)
                         for layer_name in finetuned_layers_names]
     for layer in model.layers:
         if layer not in finetuned_layers:
             layer.trainable = False
-    if verbose:
-        model.summary()
 
     return model
 
@@ -82,35 +56,25 @@ def build_se_inception_v3(verbose=False):
     model = SEInceptionV3(include_top=False, weights='imagenet', pooling='max')
     output = Dense(128, activation='softmax', name='predictions')(model.layers[-1].output)
     model = Model(inputs=model.layers[0].input, outputs=output)
-    # finetuned_layers_names = [
-    #     'predictions']
-    # finetuned_layers = [model.get_layer(name=layer_name)
-    #                     for layer_name in finetuned_layers_names]
-    # for layer in model.layers:
-    #     if layer not in finetuned_layers:
-    #         layer.trainable = False
-    if verbose:
-        model.summary()
 
     return model
 
 
 def build_inception_v3(verbose=False):
     model = InceptionV3(include_top=False, pooling='max')
-    output = Dense(128, activation='softmax', name='predictions')(model.layers[-1].output)
+    x = Dense(1024, activation='relu', name='fc_layer1')(model.layers[-1].output)
+    x = Dropout(0.4)(x)
+    output = Dense(128, activation='softmax', name='predictions')(x)
     model = Model(inputs=model.layers[0].input, outputs=output)
     finetuned_layers_names = [
-        'conv2d_93',
-        'conv2d_86',
-        'conv2d_94',
+        # 'conv2d_94',
+        'fc_layer1',
         'predictions']
     finetuned_layers = [model.get_layer(name=layer_name)
                         for layer_name in finetuned_layers_names]
     for layer in model.layers:
         if layer not in finetuned_layers:
             layer.trainable = False
-    if verbose:
-        model.summary()
 
     return model
 
@@ -119,58 +83,25 @@ def build_se_inception_resnet_v2(verbose=False):
     model = SEInceptionResNetV2(include_top=False, weights='imagenet', pooling='max')
     output = Dense(128, activation='softmax', name='predictions')(model.layers[-1].output)
     model = Model(inputs=model.layers[0].input, outputs=output)
-    # finetuned_layers_names = [
-    #     'block8_10_conv',
-    #     'dense_87',
-    #     'dense_88',
-    #     'conv_7b',
-    #     'predictions']
-    # finetuned_layers = [model.get_layer(name=layer_name)
-    #                     for layer_name in finetuned_layers_names]
-    # for layer in model.layers:
-    #     if layer not in finetuned_layers:
-    #         layer.trainable = False
-    if verbose:
-        model.summary()
 
     return model
 
 
 def build_inception_resnet_v2(verbose=False):
     model = InceptionResNetV2(include_top=False, pooling='max')
-    output = Dense(128, activation='softmax', name='predictions')(model.layers[-1].output)
+    x = Dense(1024, activation='relu', name='fc_layer1')(model.layers[-1].output)
+    x = Dropout(0.4)(x)
+    output = Dense(128, activation='softmax', name='predictions')(x)
     model = Model(inputs=model.layers[0].input, outputs=output)
     finetuned_layers_names = [
-        # 'conv2d_189',
-        # 'conv2d_190',
-        # 'conv2d_191',
-        # 'conv2d_188',
-        # 'block8_7_conv',
-        # 'conv2d_191',
-        # 'conv2d_193',
-        # 'conv2d_194',
-        # 'block8_8_conv',
-        # 'conv2d_197',
-        # 'conv2d_192',
-        # 'conv2d_195',
-        # 'conv2d_198',
-        # 'conv2d_196',
-        # 'conv2d_199',
-        # 'block8_9_conv',
-        # 'conv2d_201',
-        # 'conv2d_202',
-        # 'conv2d_200',
-        'conv2d_203',
-        'block8_10_conv',
-        'conv_7b',
+        # 'conv_7b',
+        'fc_layer1',
         'predictions']
     finetuned_layers = [model.get_layer(name=layer_name)
                         for layer_name in finetuned_layers_names]
     for layer in model.layers:
         if layer not in finetuned_layers:
             layer.trainable = False
-    if verbose:
-        model.summary()
     
     return model
 
