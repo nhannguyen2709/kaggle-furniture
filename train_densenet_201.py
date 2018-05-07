41c2b2e6dc6c75117aec1046bf4e1531da450d7a
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-05-07 14:02:26 +0700

    Update script

--- train_densenet_201.py
+++ train_densenet_201.py
@@ -7,17 +7,14 @@ import tensorflow as tf
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 from keras.backend import tensorflow_backend as K
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras.layers import Dense, GlobalMaxPooling2D
-from keras.models import Model
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
 from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import RepeatedStratifiedKFold, StratifiedShuffleSplit
 from sklearn.utils import shuffle
 
 from data import FurnituresDatasetWithAugmentation, FurnituresDatasetNoAugmentation
-from keras_EMA import ExponentialMovingAverage
-from utils import build_densenet_201, get_image_paths_and_labels, MultiGPUModel, train_lr_schedule, finetune_lr_schedule
+from utils import build_densenet_201, get_image_paths_and_labels, MultiGPUModel
 
 
 x_from_train_images, y_from_train_images = get_image_paths_and_labels(
@@ -26,20 +23,18 @@ x_from_val_images, y_from_val_images = get_image_paths_and_labels(
     data_dir='data/validation/')
 
 sss = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=201)
-for val_index, minival_index in sss.split(x_from_val_images, y_from_val_images):
+for val_index, minival_index in sss.split(
+        x_from_val_images, y_from_val_images):
     x_from_minival_images, y_from_minival_images = x_from_val_images[
         minival_index], y_from_val_images[minival_index]
     x_from_val_images, y_from_val_images = x_from_val_images[
         val_index], y_from_val_images[val_index]
 
-# 5-fold cross-validation
-input_shape = (299, 299)
+input_shape = (224, 224)
 batch_size = 32
-epochs = 6
 num_workers = 4
 n_splits = 5
 n_repeats = 1
-num_gpus = 2
 rskf = RepeatedStratifiedKFold(
     n_splits=n_splits, n_repeats=n_repeats, random_state=201)
 
@@ -54,99 +49,78 @@ for train_index, test_index in rskf.split(
     print('Found {} images belonging to {} classes'.format(len(x_train), 128))
     print('Found {} images belonging to {} classes'.format(len(x_valid), 128))
     train_generator = FurnituresDatasetWithAugmentation(
-        x_train, y_train, 
+        x_train, y_train,
         batch_size=batch_size, input_shape=input_shape)
     valid_generator = FurnituresDatasetNoAugmentation(
-        x_valid, y_valid, 
+        x_valid, y_valid,
         batch_size=batch_size, input_shape=input_shape)
 
-    trainval_lr_scheduler = LearningRateScheduler(schedule=train_lr_schedule, verbose=1)
     trainval_filepath = 'checkpoint/densenet_201/trainval.fold{}.best.hdf5'.format(
         fold)
-    save_best_trainval = ExponentialMovingAverage(filepath=trainval_filepath,
-        verbose=1,
-        monitor='val_acc',
-        save_best_only=True,
-        mode='max')
-    callbacks = [trainval_lr_scheduler, save_best_trainval]
-    # # multi-gpu train
-    # base_model = build_densenet_201()
-    # # if os.path.exists(filepath):
-    # #     base_model.load_model(filepath)
-    # base_model.compile(optimizer=Adam(lr=1e-2),
-    #                    loss='categorical_crossentropy',
-    #                    metrics=['acc'])
-    # parallel_model = MultiGPUModel(base_model, gpus=num_gpus)
-    # parallel_model.compile(optimizer=Adam(lr=1e-2), loss='categorical_crossentropy', metrics=['acc'])
-    # parallel_model.fit_generator(generator=train_generator,
-    #                              epochs=epochs,
-    #                              callbacks=callbacks,
-    #                              validation_data=valid_generator,
-    #                              workers=num_workers)   
-    
-    # del train_generator, valid_generator
-    
-    # print('Found {} images belonging to {} classes'.format(len(x_from_val_images), 128))
-    # print('Found {} images belonging to {} classes'.format(len(x_from_minival_images), 128))
-    # val_generator = FurnituresDatasetWithAugmentation(
-    #     x_from_val_images, y_from_val_images, batch_size=batch_size, input_shape=input_shape)
-    # minival_generator = FurnituresDatasetNoAugmentation(
-    #     x_from_minival_images, y_from_minival_images,
-    #     batch_size=32, input_shape=input_shape)
-    # valminival_lr_scheduler = LearningRateScheduler(schedule=finetune_lr_schedule, verbose=1)
-    # valminival_filepath = 'checkpoint/densenet_201/valminival.fold{}.best.hdf5'.format(
-    #     fold)
-    # save_best_valminival = ExponentialMovingAverage(filepath=valminival_filepath,
-    #     verbose=1,
-    #     monitor='val_acc',
-    #     save_best_only=True,
-    #     mode='max')
-    # callbacks = [valminival_lr_scheduler, save_best_valminival]
-    
-    # base_model.compile(optimizer=Adam(lr=1e-3),
-    #                    loss='categorical_crossentropy',
-    #                    metrics=['acc'])
-    # parallel_model.compile(optimizer=Adam(lr=1e-3), loss='categorical_crossentropy', metrics=['acc'])
-    # parallel_model.fit_generator(generator=val_generator,
-    #                              epochs=epochs,
-    #                              callbacks=callbacks,
-    #                              validation_data=minival_generator,
-    #                              workers=num_workers)
-
-    # single-gpu train
+    save_best_trainval = ModelCheckpoint(filepath=trainval_filepath,
+                                        verbose=1,
+                                        monitor='val_acc',
+                                        save_best_only=True,
+                                        mode='max')
+    callbacks = [save_best_trainval]
+    print('Train the last Dense layer')
     model = build_densenet_201()
-    # if os.path.exists(filepath):
-    #     model.load_model(filepath)
-    model.compile(optimizer=Adam(lr=1e-2),
-                  loss='categorical_crossentropy',
-                  metrics=['acc'])
+    model.compile(optimizer=Adam(lr=1e-3, decay=1e-5), loss='categorical_crossentropy',
+                metrics=['acc'])
     model.fit_generator(generator=train_generator,
-                        epochs=epochs,
+                        epochs=5,
                         callbacks=callbacks,
                         validation_data=valid_generator,
                         workers=num_workers)
 
-    print('Found {} images belonging to {} classes'.format(len(x_from_val_images), 128))
-    print('Found {} images belonging to {} classes'.format(len(x_from_minival_images), 128))
+    print("\nFine-tune block 31 and block 32's layers")
+    K.clear_session()
+    model = load_model(trainval_filepath)
+    for i in range(1, 18):
+        model.layers[-i].trainable = True
+    trainable_count = int(
+        np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
+    print('Trainable params: {:,}'.format(trainable_count))
+    model.compile(optimizer=Adam(lr=K.get_value(model.optimizer.lr) * 0.5, decay=1e-5),
+                loss='categorical_crossentropy',
+                metrics=['acc'])
+    model.fit_generator(generator=train_generator,
+                        epochs=25,
+                        callbacks=callbacks,
+                        validation_data=valid_generator,
+                        workers=num_workers)
+
+    print('\nFine-tune on the validation set')
+    K.clear_session()
+    print(
+        'Found {} images belonging to {} classes'.format(
+            len(x_from_val_images),
+            128))
+    print(
+        'Found {} images belonging to {} classes'.format(
+            len(x_from_minival_images),
+            128))
     val_generator = FurnituresDatasetWithAugmentation(
-        x_from_val_images, y_from_val_images, batch_size=batch_size, input_shape=input_shape)
+        x_from_val_images, y_from_val_images, batch_size=batch_size, input_shape=(448, 448))
     minival_generator = FurnituresDatasetNoAugmentation(
         x_from_minival_images, y_from_minival_images,
-        batch_size=32, input_shape=input_shape)
-    valminival_lr_scheduler = LearningRateScheduler(schedule=finetune_lr_schedule, verbose=1)
+        batch_size=batch_size, input_shape=(448, 448))
     valminival_filepath = 'checkpoint/densenet_201/valminival.fold{}.best.hdf5'.format(
         fold)
-    save_best_valminival = ExponentialMovingAverage(filepath=valminival_filepath,
-        verbose=1,
-        monitor='val_acc',
-        save_best_only=True,
-        mode='max')
-    callbacks = [valminival_lr_scheduler, save_best_valminival]
-    model.compile(optimizer=Adam(lr=1e-3), loss='categorical_crossentropy', metrics=['acc'])
+    save_best_valminival = ModelCheckpoint(filepath=valminival_filepath,
+                                        verbose=1,
+                                        monitor='val_acc',
+                                        save_best_only=True,
+                                        mode='max')
+    callbacks = [save_best_valminival]
+    model = load_model(trainval_filepath)
+    model.compile(optimizer=Adam(lr=K.get_value(model.optimizer.lr) * 0.5, decay=1e-5),
+                loss='categorical_crossentropy',
+                metrics=['acc'])
     model.fit_generator(generator=val_generator,
-                          epochs=epochs,
-                          callbacks=callbacks,
-                          validation_data=minival_generator,
-                          workers=num_workers)
+                        epochs=10,
+                        callbacks=callbacks,
+                        validation_data=minival_generator,
+                        workers=num_workers)
 
     K.clear_session()
