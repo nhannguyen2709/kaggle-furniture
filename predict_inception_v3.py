37a537f2f1da0d10aa755a7123b44c7e06a1e18d
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-05-08 11:06:02 +0700

    Predict using augmented images

--- predict_inception_v3.py
+++ predict_inception_v3.py
@@ -1,37 +1,44 @@
 import numpy as np
 import pandas as pd
 import os
+from tqdm import tqdm
 
 from keras.applications.inception_v3 import InceptionV3
 from keras.layers import Dense, GlobalMaxPooling2D
 from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
 
-test_dir = 'data/test'
+test_folders = sorted(os.listdir('data/test'))
+test_dirs = [os.path.join('data/test', test_folder) for test_folder in test_folders]
 num_workers = 4
 submit_dir = 'submission/inception_v3'
 submit_filename = 'avg_train_finetune.csv'
 
 test_datagen = ImageDataGenerator(
     rescale=1. / 255)
-test_generator = test_datagen.flow_from_directory(
-    test_dir,
-    batch_size=64,
-    target_size=(299, 299),
-    class_mode='categorical',
-    shuffle=False)
 
 folds = ['trainval.fold1', 'trainval.fold2', 'trainval.fold3',
          'valminival.fold1', 'valminival.fold2', 'valminival.fold3']
 predictions = []
-for fold in folds:
-    model = InceptionV3(include_top=False)
-    x = GlobalMaxPooling2D(name='max_pool')(model.layers[-1].output)
-    x = Dense(128, activation='softmax', name='predictions')(x)
-    model = Model(inputs=model.layers[0].input, outputs=x)
-    model.load_weights('checkpoint/inception_v3/{}.best.hdf5'.format(fold))
-    fold_pred = model.predict_generator(generator=test_generator, workers=num_workers, verbose=1) 
-    predictions.append(fold_pred)
+
+for test_dir in test_dirs:
+    test_generator = test_datagen.flow_from_directory(
+        test_dir,
+        batch_size=64,
+        target_size=(299, 299),
+        class_mode='categorical',
+        shuffle=False)
+
+    for fold in tqdm(folds):
+        model = InceptionV3(include_top=False)
+        x = GlobalMaxPooling2D(name='max_pool')(model.layers[-1].output)
+        x = Dense(128, activation='softmax', name='predictions')(x)
+        model = Model(inputs=model.layers[0].input, outputs=x)
+        model.load_weights('checkpoint/inception_v3/{}.best.hdf5'.format(fold))
+        fold_pred = model.predict_generator(generator=test_generator, workers=num_workers, verbose=1) 
+        predictions.append(fold_pred)
+
+    del test_generator
 
 test_pred = np.mean(np.array(predictions), axis=0)
 test_pred = np.argmax(test_pred, axis=1)
