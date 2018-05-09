18517691f86bc748d5c3c0679b72b600e653d9d0
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-05-09 14:50:24 +0700

    Train on higher resolution images

--- train_inception_v3.py
+++ train_inception_v3.py
@@ -30,9 +30,103 @@ for val_index, minival_index in sss.split(
     x_from_val_images, y_from_val_images = x_from_val_images[
         val_index], y_from_val_images[val_index]
 
-input_shape = (299, 299)
-batch_size = 32
-num_workers = 4
+# input_shape = (299, 299)
+# batch_size = 32
+# num_workers = 4
+# n_splits = 3
+# n_repeats = 1
+# num_gpus = 2
+# rskf = RepeatedStratifiedKFold(
+#     n_splits=n_splits, n_repeats=n_repeats, random_state=4)
+
+# fold = 0
+# for train_index, test_index in rskf.split(
+#         x_from_train_images, y_from_train_images):
+#     fold += 1
+
+#     x_train, x_valid = x_from_train_images[train_index], x_from_train_images[test_index]
+#     y_train, y_valid = y_from_train_images[train_index], y_from_train_images[test_index]
+#     print('\nFold {}'.format(fold))
+#     print('Found {} images belonging to {} classes'.format(len(x_train), 128))
+#     print('Found {} images belonging to {} classes'.format(len(x_valid), 128))
+#     train_generator = FurnituresDatasetWithAugmentation(
+#         x_train, y_train,
+#         batch_size=batch_size, input_shape=input_shape)
+#     valid_generator = FurnituresDatasetNoAugmentation(
+#         x_valid, y_valid,
+#         batch_size=batch_size, input_shape=input_shape)
+
+#     trainval_filepath = 'checkpoint/inception_v3/trainval.fold{}.best.hdf5'.format(
+#         fold)
+#     save_best_trainval = ModelCheckpoint(filepath=trainval_filepath,
+#                                          verbose=1,
+#                                          monitor='val_acc',
+#                                          save_best_only=True,
+#                                          mode='max')
+#     callbacks = [save_best_trainval]
+#     print('Train the last Dense layer')
+#     model = build_inception_v3()
+#     model.compile(optimizer=Adam(lr=1e-3, decay=1e-5), loss='categorical_crossentropy',
+#                   metrics=['acc'])
+#     model.fit_generator(generator=train_generator,
+#                         epochs=5,
+#                         callbacks=callbacks,
+#                         validation_data=valid_generator,
+#                         workers=num_workers)
+
+#     print("\nFine-tune top 2 inception blocks")
+#     for layer in model.layers[249:]:
+#         layer.trainable = True
+#     trainable_count = int(
+#         np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
+#     print('Trainable params: {:,}'.format(trainable_count))
+#     model.compile(optimizer=Adam(lr=K.get_value(model.optimizer.lr) * 0.5, decay=1e-5),
+#                   loss='categorical_crossentropy',
+#                   metrics=['acc'])
+#     model.fit_generator(generator=train_generator,
+#                         epochs=25,
+#                         callbacks=callbacks,
+#                         validation_data=valid_generator,
+#                         workers=num_workers)
+
+#     K.clear_session()
+#     print('\nFine-tune on the validation set')
+#     print(
+#         'Found {} images belonging to {} classes'.format(
+#             len(x_from_val_images),
+#             128))
+#     print(
+#         'Found {} images belonging to {} classes'.format(
+#             len(x_from_minival_images),
+#             128))
+#     val_generator = FurnituresDatasetWithAugmentation(
+#         x_from_val_images, y_from_val_images, batch_size=batch_size, input_shape=input_shape)
+#     minival_generator = FurnituresDatasetNoAugmentation(
+#         x_from_minival_images, y_from_minival_images,
+#         batch_size=batch_size, input_shape=input_shape)
+#     valminival_filepath = 'checkpoint/inception_v3/valminival.fold{}.best.hdf5'.format(
+#         fold)
+#     save_best_valminival = ModelCheckpoint(filepath=valminival_filepath,
+#                                            verbose=1,
+#                                            monitor='val_acc',
+#                                            save_best_only=True,
+#                                            mode='max')
+#     callbacks = [save_best_valminival]
+#     model = load_model(trainval_filepath)
+#     model.compile(optimizer=Adam(lr=K.get_value(model.optimizer.lr) * 0.5, decay=1e-5),
+#                   loss='categorical_crossentropy',
+#                   metrics=['acc'])
+#     model.fit_generator(generator=val_generator,
+#                         epochs=10,
+#                         callbacks=callbacks,
+#                         validation_data=minival_generator,
+#                         workers=num_workers)
+#     K.clear_session()
+
+# train on high resolution images
+input_shape = (560, 560)
+batch_size = 8
+num_workers = 10
 n_splits = 3
 n_repeats = 1
 num_gpus = 2
@@ -64,31 +158,19 @@ for train_index, test_index in rskf.split(
                                          save_best_only=True,
                                          mode='max')
     callbacks = [save_best_trainval]
-    print('Train the last Dense layer')
-    model = build_inception_v3()
-    model.compile(optimizer=Adam(lr=1e-3, decay=1e-5), loss='categorical_crossentropy',
-                  metrics=['acc'])
-    model.fit_generator(generator=train_generator,
-                        epochs=5,
-                        callbacks=callbacks,
-                        validation_data=valid_generator,
-                        workers=num_workers)
 
     print("\nFine-tune top 2 inception blocks")
-    for layer in model.layers[249:]:
-        layer.trainable = True
+    model = load_model(trainval_filepath)
     trainable_count = int(
         np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
     print('Trainable params: {:,}'.format(trainable_count))
-    model.compile(optimizer=Adam(lr=K.get_value(model.optimizer.lr) * 0.5, decay=1e-5),
-                  loss='categorical_crossentropy',
-                  metrics=['acc'])
     model.fit_generator(generator=train_generator,
-                        epochs=25,
+                        epochs=15,
                         callbacks=callbacks,
                         validation_data=valid_generator,
                         workers=num_workers)
 
+    K.clear_session()
     print('\nFine-tune on the validation set')
     print(
         'Found {} images belonging to {} classes'.format(
@@ -111,13 +193,10 @@ for train_index, test_index in rskf.split(
                                            save_best_only=True,
                                            mode='max')
     callbacks = [save_best_valminival]
-    model.compile(optimizer=Adam(lr=K.get_value(model.optimizer.lr) * 0.5, decay=1e-5),
-                  loss='categorical_crossentropy',
-                  metrics=['acc'])
+    model = load_model(trainval_filepath)
     model.fit_generator(generator=val_generator,
                         epochs=10,
                         callbacks=callbacks,
                         validation_data=minival_generator,
                         workers=num_workers)
-
     K.clear_session()
