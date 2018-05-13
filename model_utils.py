b4535e8c0da60772f71520047955d1b6668deba7
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-05-13 17:22:00 +0700

    Substantial refactor

--- model_utils.py
+++ model_utils.py
@@ -1,21 +1,17 @@
 import numpy as np
 import os
 
-from keras.applications.inception_v3 import InceptionV3
 from keras.applications.densenet import DenseNet201
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.applications.inception_v3 import InceptionV3
 from keras.applications.xception import Xception
 from keras.backend import tensorflow_backend as K
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ReduceLROnPlateau
 from keras.layers import Concatenate, Dense, Dropout, GlobalMaxPooling2D
-from keras.models import Model, load_model
-from keras.optimizers import Adam, SGD
-from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.optimizers import Adam
 from keras.utils import multi_gpu_model
 
-from sklearn.model_selection import RepeatedStratifiedKFold
-
-from data import FurnituresDatasetWithAugmentation, FurnituresDatasetNoAugmentation
 from keras_squeeze_excite_network.se_densenet import SEDenseNetImageNet264
 from keras_squeeze_excite_network.se_inception_v3 import SEInceptionV3
 from keras_squeeze_excite_network.se_inception_resnet_v2 import SEInceptionResNetV2
@@ -120,79 +116,3 @@ def build_xception():
             layer.trainable = False
 
     return model
-
-
-def train_with_kfold_cv(random_state, batch_size,
-                        input_shape, x_from_train_images,
-                        y_from_train_images, model_name,
-                        num_workers, num_layers_trained,
-                        n_splits=5, n_repeats=1):
-    rskf = RepeatedStratifiedKFold(
-        n_splits=n_splits, n_repeats=n_repeats, random_state=random_state)
-
-    fold = 0
-    for train_index, test_index in rskf.split(
-            x_from_train_images, y_from_train_images):
-        fold += 1
-
-        x_train, x_valid = x_from_train_images[train_index], x_from_train_images[test_index]
-        y_train, y_valid = y_from_train_images[train_index], y_from_train_images[test_index]
-        print('\nFold {}'.format(fold))
-        print('Found {} images belonging to {} classes'.format(len(x_train), 128))
-        print('Found {} images belonging to {} classes'.format(len(x_valid), 128))
-        train_generator = FurnituresDatasetWithAugmentation(
-            x_train, y_train,
-            batch_size=batch_size, input_shape=input_shape)
-        valid_generator = FurnituresDatasetNoAugmentation(
-            x_valid, y_valid,
-            batch_size=batch_size, input_shape=input_shape)
-
-        filepath = 'checkpoint/{}/fold{}.best.hdf5'.format(model_name,
-                                                           fold)
-        save_best = ModelCheckpoint(filepath=filepath,
-                                    verbose=1,
-                                    monitor='val_acc',
-                                    save_best_only=True,
-                                    mode='max')
-        callbacks = [save_best]
-
-        print('Train the last Dense layer')
-        if os.path.exists(filepath):
-            model = load_model(filepath)
-        elif model_name == 'xception':
-            model = build_xception()
-            model.compile(optimizer=Adam(lr=1e-3, decay=1e-5), loss='categorical_crossentropy',
-                          metrics=['acc'])
-        elif model_name == 'inception_resnet_v2':
-            model = build_inception_resnet_v2()
-            model.compile(optimizer=Adam(lr=1e-3, decay=1e-5), loss='categorical_crossentropy',
-                          metrics=['acc'])
-        elif model_name == 'densenet_201':
-            model = build_densenet_201()
-            model.compile(optimizer=Adam(lr=1e-3, decay=1e-5), loss='categorical_crossentropy',
-                          metrics=['acc'])
-        model.fit_generator(generator=train_generator,
-                            epochs=5,
-                            callbacks=callbacks,
-                            validation_data=valid_generator,
-                            workers=num_workers)
-
-        K.clear_session()
-
-        print("\nFine-tune previous blocks")
-        model = load_model(filepath)
-        for i in range(1, num_layers_trained):
-            model.layers[-i].trainable = True
-        trainable_count = int(
-            np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
-        print('Trainable params: {:,}'.format(trainable_count))
-        model.compile(optimizer=Adam(lr=K.get_value(model.optimizer.lr) * 0.5, decay=1e-5),
-                      loss='categorical_crossentropy',
-                      metrics=['acc'])
-        model.fit_generator(generator=train_generator,
-                            epochs=25,
-                            callbacks=callbacks,
-                            validation_data=valid_generator,
-                            workers=num_workers)
-
-        K.clear_session()
---
+++ retrain.py
@@ -0,0 +1,90 @@
+import argparse
+import numpy as np
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+from keras.callbacks import ReduceLROnPlateau
+from keras.models import load_model
+from keras.optimizers import Adam
+
+from sklearn.model_selection import train_test_split
+
+from data import FurnituresDatasetWithAugmentation, FurnituresDatasetNoAugmentation, get_image_paths_and_labels
+from keras_EMA import ExponentialMovingAverage
+
+parser = argparse.ArgumentParser(
+    description='Retraining')
+parser.add_argument(
+    '--batch-size',
+    default=8,
+    type=int,
+    metavar='N',
+    help='mini-batch size')
+parser.add_argument(
+    '--input-shape',
+    nargs='+',
+    type=int)
+parser.add_argument(
+    '--model-name',
+    type=str,
+    help='model to be retrained')
+parser.add_argument(
+    '--num-workers',
+    default=4,
+    type=int,
+    metavar='N',
+    help='maximum number of processes to spin up')
+
+
+def retrain_for_k_iterations(batch_size,
+                        input_shape, merged_x,
+                        merged_y, model_name,
+                        num_workers,
+                        n_iters=5):
+    for iter in range(1, n_iters + 1):
+        x_train, x_valid, y_train, y_valid = train_test_split(merged_x, merged_y, test_size=0.005)
+        print('\nIteration {}'.format(iter))
+        print('Found {} images belonging to {} classes'.format(len(x_train), 128))
+        print('Found {} images belonging to {} classes'.format(len(x_valid), 128))
+        train_generator = FurnituresDatasetWithAugmentation(
+            x_train, y_train,
+            batch_size=batch_size, input_shape=input_shape)
+        valid_generator = FurnituresDatasetNoAugmentation(
+            x_valid, y_valid,
+            batch_size=batch_size, input_shape=input_shape)
+
+        filepath = 'checkpoint/{}/iter{}.hdf5'.format(model_name,
+                                                           iter)
+        save_best = ExponentialMovingAverage(filepath=filepath,
+                                        verbose=1,
+                                        monitor='val_acc',
+                                        save_best_only=True,
+                                        mode='max')
+        reduce_lr = ReduceLROnPlateau(monitor='val_acc',
+            factor=0.2,
+            patience=2)
+        callbacks = [save_best, reduce_lr]
+
+        model = load_model(filepath)
+        model.compile(optimizer=Adam(lr=1e-3, decay=1e-5),
+                    loss='categorical_crossentropy',
+                    metrics=['acc'])
+        model.fit_generator(generator=train_generator,
+                    epochs=20,
+                    callbacks=callbacks,
+                    validation_data=valid_generator,
+                    workers=num_workers)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+
+    x_from_train_images, y_from_train_images = get_image_paths_and_labels(
+    data_dir='data/train/')
+    x_from_val_images, y_from_val_images = get_image_paths_and_labels(
+        data_dir='data/validation/')
+    merged_x, merged_y = np.concatenate((x_from_train_images, x_from_val_images)), np.concatenate((y_from_train_images, y_from_val_images))
+
+    retrain_for_k_iterations(args.batch_size, tuple(args.input_shape),
+                        merged_x, merged_y, args.model_name, args.num_workers)
