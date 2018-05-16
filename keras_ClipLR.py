48e478e876a747ecd3acefe5fe2c30b3c2d63279
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-05-16 19:17:42 +0700

    Callback to clip learning rate back to beginning learning rate

---
+++ keras_ClipLR.py
@@ -0,0 +1,16 @@
+from keras import backend as K
+from keras.callbacks import Callback
+
+class ClipLR(Callback):
+    def __init__(self, verbose=0):
+        self.verbose = verbose
+        super(ClipLR, self).__init__()
+
+    def on_epoch_begin(self, epoch, logs=None):
+        old_lr = K.get_value(self.model.optimizer.lr)
+        if old_lr < 1e-8:
+            new_lr = 0.00003
+            K.set_value(self.model.optimizer.lr, new_lr)
+            if self.verbose > 0:
+                print('\nEpoch %05d: ClipLR clipping learning '
+                        'rate to %s.' % (epoch + 1, new_lr))
