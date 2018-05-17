550da6cef8c2d320d398a3b51699f71899e78624
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-05-17 09:38:51 +0700

    Clipped lr as a parameter

--- keras_ClipLR.py
+++ keras_ClipLR.py
@@ -2,15 +2,15 @@ from keras import backend as K
 from keras.callbacks import Callback
 
 class ClipLR(Callback):
-    def __init__(self, verbose=0):
+    def __init__(self, new_lr, verbose=0):
         self.verbose = verbose
+        self.new_lr = new_lr
         super(ClipLR, self).__init__()
 
-    def on_epoch_begin(self, epoch, logs=None):
+    def on_epoch_end(self, epoch, logs=None):
         old_lr = K.get_value(self.model.optimizer.lr)
         if old_lr < 1e-7:
-            new_lr = 0.00003
-            K.set_value(self.model.optimizer.lr, new_lr)
+            K.set_value(self.model.optimizer.lr, self.new_lr)
             if self.verbose > 0:
                 print('\nEpoch %05d: ClipLR clipping learning '
-                        'rate to %s.' % (epoch + 1, new_lr))
+                        'rate to %s.' % (epoch + 1, self.new_lr))
